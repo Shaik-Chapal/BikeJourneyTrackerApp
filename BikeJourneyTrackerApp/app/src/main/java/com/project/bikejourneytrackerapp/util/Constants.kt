4a086e1cf4fcb2eabbeef3e5c340c0ddeefd1029
shaik-chapal <70383236+Shaik-Chapal@users.noreply.github.com>
2023-07-23 00:04:27 +0600

    url-create

---
+++ BikeJourneyTrackerApp/app/src/main/java/com/project/bikejourneytrackerapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.project.bikejourneytrackerapp.util
+
+class Constants {
+    companion object{
+        const val BASE_URL = "http://www.api.skygreenblue.xyz/"
+        const val PATH = "api/Journey?pageSize=10&pageNumber=1"
+
+    }
+}
